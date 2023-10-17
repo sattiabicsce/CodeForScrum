9966253444a9b0bf609ba1ad002a477e61d6a560
trpatel4 <trpatel4@ncsu.edu>
2023-10-17 13:41:29 -0400

    Fixed issue #23

--- src/ProjectList.java
+++ src/ProjectList.java
@@ -1,3 +1,29 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class ProjectList {
     
+private ArrayList<Project> project;
+private static ProjectList projectList;
+    
+    private ProjectList() {
+    }
+
+    public static ProjectList getInstance() {
+        return projectList;
+    }
+
+    public boolean addProject(Project project) {
+        return false;
+    }
+
+    // Method to remove a project from the list
+    public boolean removeProject(Project project) {
+        return false;
+    }
+
+    // Method to get a project from the list
+    public Project getProject(String projectName) {
+        return projectName;
+    }
 }
