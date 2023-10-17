a9be579bd4dafa3bdcb5f65c607a6c904c0451cf
trpatel4 <trpatel4@ncsu.edu>
2023-10-17 13:53:46 -0400

    Fixed Facade Class Stub

--- src/Facade.java
+++ src/Facade.java
@@ -1,3 +1,23 @@
 public class Facade {
+    private User currentUser;
+    private Project currentProject;
 
+    public User login(String username, String password) {
+        return currentUser;
+    }
+
+    public User signUp(String username, String password, String firstName, String lastName, String occupation) {
+        return currentUser;
+    }
+
+    public Project createProject(String name) {
+        return currentProject;
+    }
+
+    public boolean addTask(Task task) {
+        return false;
+    }
+
+    public void addComment(String note) {
+    }
 }
