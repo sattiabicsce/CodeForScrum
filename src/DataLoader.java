85e153b7d4836ca9d85afb0f2b410d778d2d4132
Brooks <bslaski@email.sc.edu>
2023-10-24 14:08:08 -0400

    data loader

--- src/DataLoader.java
+++ src/DataLoader.java
@@ -20,12 +20,12 @@ public class DataLoader extends DataConstants{
 			for(int i=0; i < peopleJSON.size(); i++) {
 				JSONObject personJSON = (JSONObject)peopleJSON.get(i);
 				UUID id = UUID.fromString((String)personJSON.get(USER_ID));
-				String userName = (String)personJSON.get(USER_USERNAME);
+				String username = (String)personJSON.get(USER_USERNAME);
                 String password = (String)personJSON.get(USER_PASSWORD);
 				String firstName = (String)personJSON.get(USER_FIRST_NAME);
 				String lastName = (String)personJSON.get(USER_LAST_NAME);
 				
-				users.add(new User(id, userName, password, firstName, lastName));
+				users.add(new User(id, username, password, firstName, lastName));
 			}
 			
 			return users;
